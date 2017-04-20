218c23e69b0cf565f7005452c0925175f6568b8e
rileydu24 <riley.du@gmail.com>
2017-04-20 16:56:40 -0700

    Checking to follow standard coding rules

--- project1.OOP.Design/entertainment_center.py
+++ project1.OOP.Design/entertainment_center.py
@@ -1,28 +1,61 @@
 import media as me
 import fresh_tomatoes as ft
 
-prison_break = me.TVShow("Prison Break", "Brothers breaking out of prison",
-"https://goo.gl/ND6CmD", "https://www.youtube.com/watch?v=x9T-9fZn_oA", "Paul Scheuring", 80)
 
-toy_story = me.Media("Toy story", "Stories with toys", "https://goo.gl/c4EaUH", 
-"https://www.youtube.com/watch?v=ZZv1vki4ou4", "John Lasseter" )
+prison_break = me.TVShow(
+    "Prison Break",
+    "Brothers breaking out of prison",
+    "https://goo.gl/ND6CmD",
+    "https://www.youtube.com/watch?v=x9T-9fZn_oA",
+    "Paul Scheuring", 80)
 
-avatar = me.Media("Avatar", "A marine on an alien planet", "https://goo.gl/3g480a", 
-"https://www.youtube.com/watch?v=5PSNL1qE6VY", "James Cameron")
+toy_story = me.Media(
+    "Toy story",
+    "Stories with toys",
+    "https://goo.gl/c4EaUH",
+    "https://www.youtube.com/watch?v=ZZv1vki4ou4",
+    "John Lasseter")
 
-school_of_rock = me.Media("School of Rock", "Rock band at school", "https://goo.gl/5lUJn8",
-"https://www.youtube.com/watch?v=oP7kExN8LFA", "Richard Linklater")
+avatar = me.Media(
+    "Avatar",
+    "A marine on an alien planet",
+    "https://goo.gl/3g480a",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY",
+    "James Cameron")
 
-the_wind_rises = me.ForeignMovies("The Wine Rises", "A look at the life of Jiro Horikoshi","https://goo.gl/CSb8pO", 
-"https://www.youtube.com/watch?v=imtdgdGOB6Q", "Hayao Miyazaki", "Japan")
+school_of_rock = me.Media(
+    "School of Rock",
+    "Rock band at school",
+    "https://goo.gl/5lUJn8",
+    "https://www.youtube.com/watch?v=oP7kExN8LFA",
+    "Richard Linklater")
 
-princess_mononoke = me.ForeignMovies("Princess Mononoke", "A tale of pricess Mononoke","https://goo.gl/ReMyvw",
-"https://www.youtube.com/watch?v=4OiMOHRDs14", "Hayao Miyazaki", "Japan")
+the_wind_rises = me.ForeignMovies(
+    "The Wine Rises",
+    "A look at the life of Jiro Horikoshi",
+    "https://goo.gl/CSb8pO",
+    "https://www.youtube.com/watch?v=imtdgdGOB6Q",
+    "Hayao Miyazaki",
+    "Japan")
 
-arriety = me.ForeignMovies("The Secret World of Arriety", "A little human beings","https://goo.gl/iO17L5",
-"https://www.youtube.com/watch?v=VlMe7PavaRQ", "Hayao Miyazaki", "Japan")
+princess_mononoke = me.ForeignMovies(
+    "Princess Mononoke",
+    "A tale of pricess Mononoke",
+    "https://goo.gl/ReMyvw",
+    "https://www.youtube.com/watch?v=4OiMOHRDs14",
+    "Hayao Miyazaki",
+    "Japan")
 
-listOfMovies = [prison_break, toy_story, avatar, school_of_rock, the_wind_rises, arriety]
+arriety = me.ForeignMovies(
+    "The Secret World of Arriety",
+    "A little human beings",
+    "https://goo.gl/iO17L5",
+    "https://www.youtube.com/watch?v=VlMe7PavaRQ",
+    "Hayao Miyazaki",
+    "Japan")
+
+listOfMovies = [
+    prison_break, toy_story, avatar, school_of_rock,
+    the_wind_rises, arriety]
 
 ft.open_movies_page(listOfMovies)
-
--- project1.OOP.Design/media.py
+++ project1.OOP.Design/media.py
@@ -1,5 +1,6 @@
 import webbrowser as wb
 
+
 class Media():
     """
     Abstract class Media contains information about movie's name, description
@@ -12,10 +13,13 @@ class Media():
         self.pic = pic
         self.trailer = trailer
         self.director = director
+
     def show_trailer(self):
         wb.open(self.trailer)
+
     def toString(self):
-        print(self.name + " is directed by " + self.director + ".")
+        return(self.name + " is directed by " + self.director + ".")
+
 
 class ForeignMovies(Media):
     """
@@ -26,21 +30,23 @@ class ForeignMovies(Media):
     def __init__(self, name, desc, pic, trailer, director, country):
         Media.__init__(self, name, desc, pic, trailer, director)
         self.country = country
-    
+
     def toString(self):
-        return (self.name + " is originally directed by " + self.director +\
-        " in "+ self.country + ".")
+        return (
+            self.name + " is originally directed by " +
+            self.director + " in " + self.country + ".")
+
 
 class TVShow(Media):
     """
     Inherited from Media class
-    Contain a new attribute:
-    episodes: number of episodes 
+    Contain a new attribute: (episodes) number of episodes
     """
     def __init__(self, name, desc, pic, trailer, director, episodes):
         Media.__init__(self, name, desc, pic, trailer, director)
         self.episodes = episodes
-    
+
     def toString(self):
-        return (self.name + " is directed by " + self.director +\
-        " and it has "+ str(self.episodes) + ".")
+        return (
+            self.name + " is directed by " +
+            self.director + " and it has " + str(self.episodes) + ".")

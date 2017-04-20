bdb7ef8ef59c584a2b20b1bbfdcb578763290b83
rileydu24 <riley.du@gmail.com>
2017-04-19 20:24:35 -0700

    Create media file, contains Media class

---
+++ project1.OOP.Design/media.py
@@ -0,0 +1,46 @@
+import webbrowser as wb
+
+class Media():
+    """
+    Abstract class Media contains information about movie's name, description
+    a picture, a trailer link and director name
+    Assuming the media is made in USA
+    """
+    def __init__(self, name, desc, pic, trailer, director):
+        self.name = name
+        self.desc = desc
+        self.pic = pic
+        self.trailer = trailer
+        self.director = director
+    def show_trailer(self):
+        wb.open(self.trailer)
+    def toString(self):
+        print(self.name + " is directed by " + self.director + ".")
+
+class ForeignMovies(Media):
+    """
+    Inherited from Media class
+    Contain a new attribute:
+    country: where the movie is originally made from
+    """
+    def __init__(self, name, desc, pic, trailer, director, country):
+        super.__init__(name, desc, pic, trailer, director)
+        self.country = country
+    
+    def toString(self):
+        print(self.name + " is originally directed by " + self.director +\
+        " in "+ self.country + ".")
+
+class TVShow(Media):
+    """
+    Inherited from Media class
+    Contain a new attribute:
+    episodes: number of episodes 
+    """
+    def __init__(self, name, desc, pic, trailer, director, episodes):
+        super.__init__(name, desc, pic, trailer, director)
+        self.episodes = episodes
+    
+    def toString(self):
+        print(self.name + " is directed by " + self.director +\
+        " and it has "+ str(self.episodes) + ".")
